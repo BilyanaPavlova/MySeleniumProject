13b1953b6db0be24949716ed83f58ff4d6f9cf50
BilyanaPavlova <bilyana.aneva@gmail.com>
2025-08-25 15:24:39 +0300

    Adding maven dependency for WebDriverManager to fetch automatically the latest driver according to the current Chrome version, that is automatically updated by the system

--- src/test/java/org/automation/BaseTest.java
+++ src/test/java/org/automation/BaseTest.java
@@ -1,9 +1,11 @@
 package org.automation;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,8 +24,19 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
 
-        System.setProperty("webdriver.chrome.driver", "/home/bili/Documents/Chromedriver/chromedriver-linux64/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "/home/bili/Documents/Chromedriver/chromedriver-linux64/chromedriver");
+
+        // Using WebDriverManager to manage driver binaries are always up to date with the Chrome browser version
+        // that is automatically downloaded by the system.
+        // Then we do not need to manually download and set the path for the System as shown above.
+        WebDriverManager manager = WebDriverManager.chromedriver();
+        manager.setup();
         driver = new ChromeDriver();
+
+        // If we test on iOS Safari browser... etc for different browsers
+//        WebDriverManager iOSmanager = WebDriverManager.safaridriver();
+//        iOSmanager.setup();
+//        driver = new SafariDriver();
 
         driver.get("https://www.saucedemo.com/");
 
--- src/test/java/org/automation/LoginTests.java
+++ src/test/java/org/automation/LoginTests.java
@@ -94,6 +94,7 @@ public class LoginTests extends BaseTest {
     }
 
 
+
 }
 
 
